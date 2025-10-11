8786621f1a8e46ebfb7cb82bb214ff9136b3fc51
timur <tymatee@gmail.com>
2025-10-11 13:23:36 +0300

    Formatting

--- os_wrapper/threads_unix.cpp
+++ os_wrapper/threads_unix.cpp
@@ -1,34 +1,32 @@
 #include <pthread.h>
 #include <system_error>
-#include<iostream>
 
 #include <thread.hpp>
 
 namespace OSWrapper {
-struct Thread::OSSpecificData{
-	pthread_t pthread;
+struct Thread::OSSpecificData {
+  pthread_t pthread;
 };
-Thread::Thread(ThreadFunc func) : func_(func), data_(new OSSpecificData()) {
-}
+Thread::Thread(ThreadFunc func) : func_(func), data_(new OSSpecificData()) {}
 void Thread::Run(void *thread_arg) {
   int err = pthread_create(&(data_->pthread), NULL, func_, thread_arg);
   if (err != 0) {
     throw std::system_error(std::error_code(err, std::system_category()));
   }
 }
-void Thread::Join(){
+void Thread::Join() {
   int err = pthread_join(data_->pthread, NULL);
   if (err != 0) {
     throw std::system_error(std::error_code(err, std::system_category()));
   }
 }
-void Thread::Detach(){
+void Thread::Detach() {
   int err = pthread_detach(data_->pthread);
   if (err != 0) {
     throw std::system_error(std::error_code(err, std::system_category()));
   }
 }
-Thread::~Thread(){};
-Thread::Thread(const Thread& other): func_(other.func_), data_(new OSSpecificData()){
-}
+Thread::~Thread() {};
+Thread::Thread(const Thread &other)
+    : func_(other.func_), data_(new OSSpecificData()) {}
 } // namespace OSWrapper
